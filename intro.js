115ef3c674fa82e72bf118831df7bc57912a5967
mendox0220 <mendox0220@gmail.com>
2019-08-12 12:18:22 +0900

    first commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+function openLNav() {
+    document.getElementById("LeftSidenav").style.width = "250px";
+}
+
+function closeLNav() {
+    document.getElementById("LeftSidenav").style.width = "0";
+}
