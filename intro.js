87b44e3c9330dd0f228fcc97b7c2a3c8a8270021
mendox0220 <mendox0220@gmail.com>
2019-08-12 15:24:28 +0900

    main front

--- intro.js
+++
@@ -1,7 +0,0 @@
-function openLNav() {
-    document.getElementById("LeftSidenav").style.width = "250px";
-}
-
-function closeLNav() {
-    document.getElementById("LeftSidenav").style.width = "0";
-}
